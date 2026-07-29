ed7306f787fc0a5c0a8409340b58e727a892d132
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-29 23:45:28 +0530

    03_inheritance.py

--- 03_inheritance.py
+++ 03_inheritance.py
@@ -1,1 +1,30 @@
+#Inheritance - allow a class to inherit attibutes and objects from another class
 
+class Animals:
+  def __init__(self,name,category):
+    self.name = name
+    self.category = category
+
+  def eat(self):
+    print(f"{self.name} is eating")
+
+  def sleep(self):
+      print(f"{self.name} is sleeping")
+
+class Dog(Animals):
+
+  def speak(self):
+    print("WOOF!")
+
+class Cat(Animals):
+  def speak(self):
+    print("SQUEEK!")
+   
+
+dog = Dog("Dog","pet")
+dog.eat()
+dog.speak()
+
+cat = Cat("cat","pet")
+cat.sleep()
+cat.speak()
