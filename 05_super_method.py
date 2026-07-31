448c13313617e93c7c892e83e0f51d144d1a536b
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-07-31 23:56:44 +0530

    super_method.py

--- 05_super_method.py
+++ 05_super_method.py
@@ -1,1 +1,35 @@
+#super class -  inherit method of super class to sub class
 
+
+class Shape:
+   def __init__(self,color,is_filled):
+      self.color = color
+      self.is_filled = is_filled
+
+
+
+
+class Circle(Shape):
+  def __init__(self,color,is_filled,radius):
+    super().__init__(color , is_filled)
+    self.radius = radius
+
+
+class Sqaure(Shape):
+  def __init__(self,color,is_filled,width):
+      super().__init__(color , is_filled) 
+      self.width = width
+  
+
+
+class Triangle(Shape):
+  def __init__(self,color,is_filled,width,height):
+      super().__init__(color , is_filled)
+      self.width = width
+      self.height = height
+
+
+circle = Circle("red",True,8)
+print(circle.color)
+print(circle.is_filled)
+print(circle.radius)
