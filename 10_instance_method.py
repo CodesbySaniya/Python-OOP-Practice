18cd1abe2f4a5195d92d36735feeddc0f397d83e
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-08-04 22:34:50 +0530

    instance_method.py

--- 10_instance_method.py
+++ 10_instance_method.py
@@ -1,1 +1,14 @@
+#methods 
 
+class Employee:
+
+  def __init__(self,name,position):
+    self.name = name
+    self.position = position
+
+  def get_info(self):
+    return f" {self.name} = {self.position}"  #objects in class is known as Instance method
+
+
+employee = Employee("saniya","manager")
+print(employee.get_info())
