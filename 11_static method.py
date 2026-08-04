948910c8af541d830b325719157726a37ec4503a
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-08-04 22:36:00 +0530

    Update 11_static method.py

--- 11_static method.py
+++ 11_static method.py
@@ -1,1 +1,22 @@
+#methods 
+
+class Employee:
+
+  def __init__(self,name,position):
+    self.name = name
+    self.position = position
+
+  def get_info(self):
+    return f" {self.name} = {self.position}"  #objects in class is known as Instance method
+
+  @staticmethod
+  def is_valid_position(position):   #static method does not belong to class objects
+    valid_position = ["manager","cook","cashier"]
+    return position in valid_position
+
+
+print(Employee.is_valid_position("cook"))
+
+employee = Employee("saniya","manager")
+print(employee.get_info)
 
