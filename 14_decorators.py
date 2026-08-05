d6b90e00ab3fb49a68804e2ee973c20b0ce99b34
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-08-05 23:21:35 +0530

    Update 14_decorators.py

--- 14_decorators.py
+++ 14_decorators.py
@@ -1,1 +1,23 @@
+import time
 
+def timer(func):
+
+    def wrapper():
+        start = time.time()
+
+        func()
+
+        end = time.time()
+
+        print(end - start)
+
+    return wrapper
+
+
+@timer
+def work():
+
+    print("Working...")
+    time.sleep(2)
+
+work()
