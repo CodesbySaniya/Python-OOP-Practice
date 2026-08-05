7ccd99f51121057fd741e6703fb712b92851ad09
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-08-05 22:56:39 +0530

    magic_method.py

--- 13_magic_method.py
+++ 13_magic_method.py
@@ -15,11 +15,18 @@ class Book:
   def __eq__(self, value):
     return self.title == value.title and self.author == value.author
 
+  def __lt__(self,other):                     #less than function to compare
+    return self.num_pages < other.num_pages
 
+  def __gt__(self,other):                     #greater than function to compare
+    return self.num_pages > other.num_pages
+  
+  
 book1 = Book("abc"," thomsan",90)
 book2= Book("xyz","vex king",109)
 book3 = Book("abc"," thomsan",90)
 print(book1)
 print(book2)
 print(book1==book3)     #both are same but not giving output as True so we use __eq__
-
+print(book1 < book2)    #for less than
+print(book1>book2)      #for greater than
